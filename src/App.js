f57d65fb97d79c03e083dc4fb06dbfe47ff6abbb
sharath V <sharathkrml@gmail.com>
2022-02-07 15:33:14 +0530

    added scss,changed title using componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import React, { Component } from 'react';
+import "./App.scss";
+import React, { Component } from "react";
 
 export default class App extends Component {
+  componentDidMount() {
+    document.title = "Rock Paper Scissors";
+  }
   render() {
     return <div>Hello world</div>;
   }
