7de6d342b1be847713e672605169216d72cff172
sharath V <sharathkrml@gmail.com>
2022-02-07 16:50:04 +0530

    finished header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.scss";
 import React, { Component } from "react";
-
+import logo from "./assets/logo.svg";
 export default class App extends Component {
   componentDidMount() {
     document.title = "Rock Paper Scissors";
   }
   render() {
-    return <div>Hello world</div>;
+    return (
+      <div className="App">
+        <header className="header">
+          <img src={logo} alt="logo" />
+          <div className="score-card">
+            <div className="score-title">SCORE</div>
+            <div className="score-value">12</div>
+          </div>
+        </header>
+      </div>
+    );
   }
 }
