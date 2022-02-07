f161f81d16bf3bd2359791deee8b08cf1e290283
sharath V <sharathkrml@gmail.com>
2022-02-07 22:13:32 +0530

    added mobile modal

--- src/App.js
+++ src/App.js
@@ -4,15 +4,49 @@ import logo from "./assets/logo.svg";
 import paper from "./assets/icon-paper.svg";
 import scissors from "./assets/icon-scissors.svg";
 import rock from "./assets/icon-rock.svg";
+import rules from "./assets/image-rules.svg";
+import closeBtn from "./assets/icon-close.svg";
 
 import Choice from "./Components/Choice";
 export default class App extends Component {
+  state = {
+    showRule: false,
+  };
+  toggleRule = () => {
+    this.setState((prevState) => ({ showRule: !prevState.showRule }));
+    // console.log("web");
+  };
   componentDidMount() {
     document.title = "Rock Paper Scissors";
   }
   render() {
+    const { showRule } = this.state;
     return (
       <div className="App">
+        {showRule && (
+          <div className="rules-modal">
+            <div className="rules-header">
+              <h2>RULES</h2>
+              <img
+                className="btn-fullScreen"
+                onClick={this.toggleRule}
+                src={closeBtn}
+                alt="close"
+              />
+            </div>
+            <div className="rule-wrapper">
+              <img className="rule-img" src={rules} alt="rules" />
+            </div>
+            <div className="rule-wrapper">
+              <img
+                className="btn-mobile"
+                onClick={this.toggleRule}
+                src={closeBtn}
+                alt="close"
+              />
+            </div>
+          </div>
+        )}
         <header className="header">
           <img src={logo} alt="logo" />
           <div className="score-card">
@@ -31,7 +65,9 @@ export default class App extends Component {
           </div>
         </main>
         <footer className="footer">
-          <button className="rules">RULES</button>
+          <button onClick={this.toggleRule} className="rules">
+            RULES
+          </button>
         </footer>
       </div>
     );
